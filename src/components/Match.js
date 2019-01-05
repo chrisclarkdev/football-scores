e9d18368419f6b0fc315c7b4175da5746086b03c
chrisclarkdev <chrisclark@posteo.net>
2019-01-05 01:12:02 +0000

    figured out the logic for scores and sorted out heading and colour

--- src/components/Match.js
+++ src/components/Match.js
@@ -5,12 +5,11 @@ const Match = props =>{
   return(
 
     <div className="flex-container">
+    <h1 className="leagueHeading">{props.matches[0] && props.matches[0].league_name}</h1>
+     <ul className="match_ul">
       {props.matches.map(matchGames => (
         
-        <ul key={matchGames.match_id} className="match_ul">
-        <div className="leagueHeading">
-        <h3>{matchGames.country_name} : {matchGames.league_name}</h3>
-        </div>
+       <div key={matchGames.match_id}>
           <li className="match_li">
            <div className="hometeam teamName"> 
              <h2> 
@@ -19,24 +18,23 @@ const Match = props =>{
            </div>
            <div className="scores"> 
              <h2>
-              {matchGames.match_status === "" 
-                ? `${matchGames.match_date}` 
-             : `${matchGames.match_hometeam_score}` }</h2>
+              {matchGames.match_status !== "" 
+                ? `${matchGames.match_hometeam_score} : ${matchGames.match_awayteam_score}` : `${matchGames.match_date}` }</h2>
            </div>
            <div className="awayteam teamName">
              <h2> 
                {matchGames.match_awayteam_name.split('amp;')}
              </h2>
            </div>
-
+           {matchGames.statistics[2] !== "" ?
+            `<h5>Possession {matchGames.statistics[2]&& matchGames.statistics[2].home}%</h5>
            <h5 className="match_status">{matchGames.match_status}</h5>
-
-
-
+           <h5>Possession {matchGames.statistics[2]&& matchGames.statistics[2].away}%</h5>` : "" }
 
           </li>
-        </ul>
+        </div>
       ))}
+      </ul>
     </div>
 
   );
