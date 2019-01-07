cc0e19219eeaaefc45ff2876244265816eab5da1
chrisclarkdev <chrisclark@posteo.net>
2019-01-07 15:49:05 +0000

    not sure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Match from "./components/Match";
 import "./App.css";
 import axios from "axios";
-const dateFrom = "2019-1-6";
-const dateTo = "2019-1-6";
-const league_id = "69";
+const dateFrom = "2019-1-1";
+const dateTo = "2019-1-13";
+const league_id = "62";
 const URL = `https://apifootball.com/api/?action=get_events&from=${dateFrom}&to=${dateTo}&league_id=${league_id}&APIkey=1a9df061406d7abfccff89c4c2b15f26060d0a08d9091b1dc4a0e418a0f70158`;
 
 class App extends Component {
@@ -16,6 +16,7 @@ class App extends Component {
     axios.get(URL).then(res => {
       const events = res.data;
       this.setState({ events });
+      console.log(res.request.status);
     });
   }
   render() {
@@ -25,7 +26,6 @@ class App extends Component {
           <h1>Football Scores</h1>
         </header>
         <Match matches={this.state.events} />
-        {console.log(this.state.events.map(heading => heading))}
       </div>
     );
   }
